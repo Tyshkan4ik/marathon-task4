d89e750f62b004f20b02461ab0eb0448b674a183
Tyshkan4ik <001vat@gmail.com>
2023-03-10 10:54:07 +0300

    Initial Commit

---
+++ Marathon task-4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marathon task-4
+//
+//  Created by Виталий Троицкий on 10.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
